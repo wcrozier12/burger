a83c43b253943d34f835c96621c9942578b44fc7
wcrozier12 <wcc8@nau.edu>
2017-10-25 15:55:50 -0700

    edited gitignore

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,13 @@ const mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Horizon45!",
     database: "burgers_Db"
   });
 }
